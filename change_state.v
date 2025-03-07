2cc1b2439f15f89e735cef815dfba5d21c974441
minjoooo303 <156404156+minjoooo303@users.noreply.github.com>
2025-03-08 00:17:19 +0900

    Update change_state.v

--- change_state.v
+++ change_state.v
@@ -1,12 +1,13 @@
 `include "vending_machine_def.v"
 
 
-module change_state(clk,reset_n,current_total_nxt,current_total);
+module change_state(clk,reset_n,current_total_nxt,current_total,i_trigger_return);
 
 	input clk;
 	input reset_n;
 	input [`kTotalBits-1:0] current_total_nxt;
 	output reg [`kTotalBits-1:0] current_total;
+	input i_trigger_return;
 	
 	// Sequential circuit to reset or update the states
 	always @(posedge clk ) begin //since it is sequential logic, nonblocking assignment is required
@@ -14,6 +15,9 @@ module change_state(clk,reset_n,current_total_nxt,current_total);
 			// TODO: reset all states.
 			current_total <= 0;
 		end
+		if(i_trigger_return) begin 
+                current_total <=3;
+		end 
 		else begin
 			// TODO: update all states.
 			current_total <= current_total_nxt;
