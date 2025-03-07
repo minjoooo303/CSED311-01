9aa69475cc4374b92661ffcdcc95376b5cc7628a
minjoooo303 <156404156+minjoooo303@users.noreply.github.com>
2025-03-08 00:17:33 +0900

    Update check_time_and_coin.v

--- check_time_and_coin.v
+++ check_time_and_coin.v
@@ -37,13 +37,13 @@ module check_time_and_coin(clk,reset_n,i_input_coin,i_select_item,current_total,
 		if (i_input_coin != 0 && (current_total==0 || current_total==1)) begin
 			
 			if ( i_input_coin == 3'b001 ) begin
-				output_total <= input_total + coin_value[0];
+				output_total = input_total + coin_value[0];
 			end
 			else if ( i_input_coin == 3'b010 ) begin
-				output_total <= input_total + coin_value[1];
+				output_total = input_total + coin_value[1];
 			end
 			else if ( i_input_coin == 3'b100 ) begin
-				output_total <= input_total + coin_value[2];
+				output_total = input_total + coin_value[2];
 			end
 			else begin
 			end
@@ -51,16 +51,16 @@ module check_time_and_coin(clk,reset_n,i_input_coin,i_select_item,current_total,
 		else if ((i_select_item & o_available_item) && current_total==1) begin
 
 			if ( i_select_item == 4'b0001 ) begin
-				output_total <= output_total- item_price[0];
+				output_total = input_total- item_price[0];
 			end
 			else if ( i_select_item == 4'b0010 ) begin
-				output_total <= output_total - item_price[1];
+				output_total = input_total - item_price[1];
 			end
 			else if ( i_select_item == 3'b0100 ) begin
-				output_total<= output_total- item_price[2];
+				output_total = input_total- item_price[2];
 			end
 			else if(i_select_item==4'b1000) begin
-                output_total <= output_total - item_price[3];
+                output_total = input_total - item_price[3];
             end
 		end
             
@@ -69,6 +69,9 @@ module check_time_and_coin(clk,reset_n,i_input_coin,i_select_item,current_total,
 
 	always @(*) begin
 		// TODO: o_return_coin
+		o_return_coin = 3'b000;
+		return_total = input_total;
+
 		case (current_total)
 		0: begin 
 			o_return_coin=3'b000;
@@ -83,16 +86,17 @@ module check_time_and_coin(clk,reset_n,i_input_coin,i_select_item,current_total,
 			return_total=0;
 		end
 		3: begin
-			if (input_total >= coin_value[2]) begin
+			return_total = input_total;
+			if (return_total >= coin_value[2]) begin
             	o_return_coin = 3'b100;
 				return_total = input_total -coin_value[2];
 			end
-        	else if (input_total >= coin_value[1]) begin 
-            	o_return_coin = 3'b010;
+        	if (return_total >= coin_value[1]) begin 
+            	o_return_coin = o_return_coin | 3'b010;
 				return_total = input_total -coin_value[1];
 			end
-        	else if (input_total >= coin_value[0])begin
-            	o_return_coin = 3'b001;
+        	else if (return_total >= coin_value[0])begin
+            	o_return_coin = o_return_coin | 3'b001;
 				return_total = input_total -coin_value[0];
 			end
         	else begin
@@ -118,11 +122,11 @@ module check_time_and_coin(clk,reset_n,i_input_coin,i_select_item,current_total,
 		end
 		else begin
 		// TODO: update all states.
-			if (!return_total) begin
-				input_total <= output_total;
+			if (return_total>0) begin
+				input_total <= return_total;
 			end
 			else begin
-				input_total <= return_total;
+				input_total <= output_total;
 			end
 			wait_time <= wait_time - 1;
 			if(i_trigger_return) begin 
