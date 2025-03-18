8d0798e0ac881bf8dc85cddfd3bdddb01173e110
minjoooo303 <156404156+minjoooo303@users.noreply.github.com>
2025-03-18 21:47:31 +0900

    Create mux.v

---
+++ lab2/mux.v
@@ -0,0 +1,15 @@
+module mux(input s,
+           input [31:0] in1,
+           input [31:0] in0,
+           output reg [31:0] out);
+
+always @(*) begin
+    if(s) begin
+        out = in1;
+    end
+    else begin
+        out = in0;
+    end
+end
+
+endmodule
