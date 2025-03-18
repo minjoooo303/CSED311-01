9e09c4f35e3e73e137916ed18709f9efc9dfbafe
minjoooo303 <156404156+minjoooo303@users.noreply.github.com>
2025-03-18 21:47:04 +0900

    Create pc.v

---
+++ lab2/module pc (input reset,            input clk,            input [31:0] next_pc,            output reg [31:0] current_pc); /pc.v
@@ -0,0 +1,14 @@
+module pc (input reset,
+           input clk,
+           input [31:0] next_pc,
+           output reg [31:0] current_pc); // instruction at addr
+
+always @(posedge clk) begin:
+    if (reset) begin:
+        current_pc <= 0; 
+    end
+    else begin:
+        current_pc <= next_pc;
+    end
+end
+endmodule
