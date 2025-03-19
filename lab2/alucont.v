2124fbab6b1177890d4ba5592f68cf67aaaf44c0
minjoooo303 <156404156+minjoooo303@users.noreply.github.com>
2025-03-19 11:10:54 +0900

    Update alucont.v

--- lab2/alucont.v
+++ lab2/alucont.v
@@ -1,7 +1,8 @@
 `include "opcodes.v"
 
 module alucont (input [10:0] part_of_inst, 
-                output [3:0] Reg alu_op
+                output reg[3:0] alu_op,
+                output reg [1:0] btype
 );
 
 wire [6:0] opcodes;
@@ -13,44 +14,57 @@ assign funct3 = part_of_inst[9:7];
 assign funct7 = part_of_inst[10];
 
 always @(*) begin
+    alu_op = 4'b0000;
+    btype  = 2'b00;
+    
     case (opcodes)
-        `ARITHMETIC, `ARITHMETIC_IMM: begin
-            // ARITHMETIC의 경우 funct7이 1이면 추가 분기 처리
-            if(opcodes == `ARITHMETIC && funct7) begin
+        `ARITHMETIC: begin
+            if(funct7) begin
                 alu_op = 4'b0001;
-            end 
+            end
             else begin
                 case(funct3)
                     `FUNCT3_ADD: alu_op = 4'b0000;
-                    `FUNCT3_AND: alu_op = 4'b0010;
-                    `FUNCT3_OR:  alu_op = 4'b0011;
-                    `FUNCT3_XOR: alu_op = 4'b0100;
-                    `FUNCT3_SLL: alu_op = 4'b0101;
-                    `FUNCT3_SRL: alu_op = 4'b0110;
+                    `FUNCT3_SLL: alu_op = 4'b1010;
+                    `FUNCT3_XOR: alu_op = 4'b1000;
+                    `FUNCT3_OR: alu_op = 4'b0101;
+                    `FUNCT3_AND: alu_op = 4'b0100;
+                    `FUNCT3_SRL: alu_op = 4'b1011;
+                    default: alu_op = 4'b0000;
+
                 endcase
             end
-        end
-        `LOAD: begin
-            alu_op = 4'b0000;
-        end
-        `JALR: begin
-            alu_op = 4'b0000;
-        end
-        `STORE: begin
-            alu_op = 4'b0000;
-        end
-        `BRANCH: begin
-            case (funct3)
-                `FUNCT3_BEQ: alu_op = 4'b1000;
-                `FUNCT3_BNE: alu_op = 4'b1001;
-                `FUNCT3_BLT: alu_op = 4'b1010;
-                `FUNCT3_BGE: alu_op = 4'b1011;
+        end 
+
+        `ARITHMETIC_IMM: begin
+            case(funct3)
+                `FUNCT3_ADD: alu_op = 4'b0000;
+                `FUNCT3_SLL: alu_op = 4'b1010;
+                `FUNCT3_XOR: alu_op = 4'b1000;
+                `FUNCT3_OR: alu_op = 4'b0101;
+                `FUNCT3_AND: alu_op = 4'b0100;
+                `FUNCT3_SRL: alu_op = 4'b1011;
+                default: alu_op = 4'b0000;
             endcase
         end
+        `LOAD: alu_op = 4'b0000;
+        `JALR: alu_op = 4'b0000;
+        `STORE: alu_op = 4'b0000;
+        `BRANCH: begin
+            alu_op = 4'b0001;
+            case(funct3)
+                `FUNCT3_BEQ: btype = 2'b00;
+                `FUNCT3_BNE: btype = 2'b01;
+                `FUNCT3_BLT: btype = 2'b10;
+                `FUNCT3_BGE: btype = 2'b11;
+                default: btype = 2'b00;
+            endcase
+        end
+        `JAL: alu_op = 4'b0000;
+        `ECALL: alu_op = 4'b0000;
+        default: alu_op = 4'b0000;
+
     endcase
-
-
 end
-
 
 endmodule
