6a74d70c63d035a74214887f8db050ec7248c3ab
Yoojin <80443883+efilofblue@users.noreply.github.com>
2025-03-19 23:10:57 +0900

    Update pc.v

--- lab2/pc.v
+++ lab2/pc.v
@@ -3,11 +3,11 @@ module pc (input reset,
            input [31:0] next_pc,
            output reg [31:0] current_pc); // instruction at addr
 
-always @(posedge clk) begin:
-    if (reset) begin:
+always @(posedge clk) begin
+    if (reset) begin
         current_pc <= 0; 
     end
-    else begin:
+    else begin
         current_pc <= next_pc;
     end
 end
