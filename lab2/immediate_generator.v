a5df89eee7be177e100023c5818a57c1a76bc62a
Yoojin <80443883+efilofblue@users.noreply.github.com>
2025-03-19 22:15:47 +0900

    Add files via upload

--- lab2/immediate_generator.v
+++ lab2/immediate_generator.v
@@ -15,10 +15,10 @@ module immediate_generator (    input [31:0] part_of_inst,
                 imm_gen_out = {{20{part_of_inst[31]}}, part_of_inst[31:25], part_of_inst[11:7]};
             end
             `BRANCH: begin // SB-type
-                imm_gen_out = {{19{part_of_inst[31]}}, part_of_inst[31], part_of_inst[7], part_of_inst[30:25], part_of_inst[11:8], 1'b0}
+                imm_gen_out = {{19{part_of_inst[31]}}, part_of_inst[31], part_of_inst[7], part_of_inst[30:25], part_of_inst[11:8], 1'b0};
             end
             `JAL: begin // J-type
-                imm_gen_out = {{11{part_of_inst[31]}}, part_of_inst[31], part_of_inst[19:12], part_of_inst[20], part_of_inst[30:21], 1'b0}
+                imm_gen_out = {{11{part_of_inst[31]}}, part_of_inst[31], part_of_inst[19:12], part_of_inst[20], part_of_inst[30:21], 1'b0};
             end
             default: begin
                 imm_gen_out = 32'b0;
