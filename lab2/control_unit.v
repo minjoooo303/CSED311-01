2985113511445a239d96ef6a86a60a1278644eeb
Yoojin <80443883+efilofblue@users.noreply.github.com>
2025-03-24 00:07:42 +0900

    Update control_unit.v

--- lab2/control_unit.v
+++ lab2/control_unit.v
@@ -8,8 +8,8 @@ module control_unit (    input [6:0] part_of_inst,
                     output wire mem_read,
                     output wire mem_to_reg,
                     output wire mem_write,
-                    output wire alu_src,
-                    output wire write_enable, // reg_write
+                    output reg alu_src,
+                    output reg write_enable, // reg_write
                     output wire pc_to_reg,
                     output reg is_ecall
                     
@@ -23,10 +23,6 @@ module control_unit (    input [6:0] part_of_inst,
     reg isStore;
     reg isLoad;
 
-    reg isRtype;
-    reg isSBtype;
-
-
     assign is_jalr = jalr_reg;
     assign is_jal = jal_reg;
     assign branch = branch_reg;
@@ -34,8 +30,6 @@ module control_unit (    input [6:0] part_of_inst,
     assign mem_read = isLoad;
     assign mem_to_reg = isLoad;
     assign mem_write = isStore;
-    assign alu_src = !isRtype || !isSBtype;
-    assign write_enable = !isStore && !isSBtype;
 
     assign pc_to_reg = PCtoReg_reg;
 
@@ -46,32 +40,39 @@ module control_unit (    input [6:0] part_of_inst,
         PCtoReg_reg = 0;
         isStore = 0;
         isLoad = 0;
-        isRtype = 0;
-        isSBtype = 0;
         is_ecall = 0;
+        alu_src = 0;
+        write_enable = 0;
 
         case(part_of_inst[6:0])
             `ARITHMETIC: begin // R-type
-                isRtype = 1;
+                write_enable = 1;
             end
             `ARITHMETIC_IMM: begin // I-type
+                alu_src = 1;
+                write_enable = 1;
             end
             `LOAD: begin // I-type
                 isLoad = 1;
+                alu_src = 1;
+                write_enable = 1;
             end
             `JALR: begin // I-type
                 jalr_reg = 1;
                 PCtoReg_reg = 1;
+                alu_src = 1;
+                write_enable = 1;
             end
             `STORE: begin // S-type
                 isStore = 1;
+                alu_src = 1;
             end
             `JAL: begin // J-type
                 jal_reg = 1;
                 PCtoReg_reg = 1; 
+                write_enable = 1;
             end
             `BRANCH: begin // B-type
-                isSBtype = 1;
                 branch_reg = 1;
             end
             `ECALL: begin
