f0e6eb582b2b3380d2f2433800ea7f48f37623e8
minjoooo303 <156404156+minjoooo303@users.noreply.github.com>
2025-04-30 22:38:39 +0900

    Update BHSR.v

--- lab4_2/BHSR.v
+++ lab4_2/BHSR.v
@@ -1,6 +1,6 @@
 module BHSR() ( 
-    input clk,
     input reset,
+    input clk,
     input update_en,       // 업데이트할지 신호
     input taken,           // 이번 분기 결과 (1: taken, 0: not taken)
     output reg [4:0] bhsr_out  // 현재 BHSR 값 출력 // WIDTH:5 기준
