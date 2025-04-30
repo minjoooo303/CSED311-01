a0896144de63f852d3b29008a0a529a3b74e74d1
minjoooo303 <156404156+minjoooo303@users.noreply.github.com>
2025-04-30 23:26:46 +0900

    Update BTB.v
    btb, table 분리

--- lab4_2/BTB.v
+++ lab4_2/BTB.v
@@ -5,13 +5,10 @@ module BTB (
             input update_BTB,
             input [31:0] real_target,
 
-            output reg BTB_hit,
             output reg [31:0] guessed_target
 );
 
-    reg [26:0] Tags [0:31];
     reg [31:0] Targets [0:31];
-    reg Valid_bit [0:31];
 
     wire [4:0] pc_idx;
     assign pc_idx = pc_in[6:2];
@@ -19,28 +16,20 @@ module BTB (
     always @(posedge clk) begin
         if (reset) begin
             for (integer i = 0; i < 32; i++) begin
-                Valid_bit[i] <= 0;
+                Targets[i] <= 0;
             end
-            BTB_hit <= 0;
+            guessed_target <= 0;
         end
         else begin
-            // 분기 발생; BTB를 업데이트한다; Tags, Targets, Valid_bit의 값을 알맞게 변경
+            // 분기 발생; BTB를 업데이트한다; Tags, Targets 값을 알맞게 변경
             if (update_BTB) begin
-                Tags[pc_idx] <= pc_in[31:7];
                 Targets[pc_idx] <= real_target;
-                Valid_bit[pc_idx] <= 1;
             end
-
-            // hit인 경우; BTB에서 target pc를 가져와 next pc로 설정
-            if (Valid_bit[pc_idx] && (Tags[pc_idx] == pc_in[31:7])) begin
-                BTB_hit <= 1;
-                guessed_target <= Targets[pc_idx];
-            end
-            // miss인 경우; flush 발생
-            else begin
-                BTB_hit <= 0;
-                guessed_target <= 32'b0;
-            end
+            // predict_target 출력
+            
+            guessed_target <= Targets[pc_idx];
+            
+            
         end
     end
 
